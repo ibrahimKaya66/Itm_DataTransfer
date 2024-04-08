d098e3ee0110040bb01dca17f5eaea3cdbd96542
İbrahim KAYA <ibrahim.kaya5466@gmail.com>
2024-04-08 12:23:21 +0300

    .

--- DataTransfer.Model/Ado.Net/OperatorPerformance.cs
+++ DataTransfer.Model/Ado.Net/OperatorPerformance.cs
@@ -2,15 +2,15 @@
 {
     public class OperatorPerformance
     {
-        public string Employee_Name { get; set; }
+        public string? Employee_Name { get; set; }
         public int EmployeeId { get; set; }
-        public string Operation_Name { get; set; }
-        public string Operation_Type { get; set; }
+        public string? Operation_Name { get; set; }
+        public string? Operation_Type { get; set; }
         public decimal TimeSecond { get; set; }
-        public string Line_Name { get; set; }
-        public string Group_Name { get; set; }
-        public string GroupCode_Name { get; set; }
-        public string Department_Name { get; set; }
+        public string? Line_Name { get; set; }
+        public string? Group_Name { get; set; }
+        public string? GroupCode_Name { get; set; }
+        public string? Department_Name { get; set; }
         public decimal Performance { get; set; }
     }
 }
--- DataTransfer.Model/Entities/Department.cs
+++ DataTransfer.Model/Entities/Department.cs
@@ -3,7 +3,7 @@
     public class Department
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int FactoryId { get; set; }
         public DateTime CreatedDate { get; set; }
     }
