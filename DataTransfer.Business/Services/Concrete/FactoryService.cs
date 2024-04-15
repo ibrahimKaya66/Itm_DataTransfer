103242ac7b160cf90f0f61d571291d6cb5887f3b
İbrahim KAYA <ibrahim.kaya5466@gmail.com>
2024-04-15 13:57:54 +0300

    .

--- DataTransfer.Business/Services/Concrete/FactoryService.cs
+++ DataTransfer.Business/Services/Concrete/FactoryService.cs
@@ -2,13 +2,38 @@
 using DataTransfer.Dal.Context;
 using DataTransfer.Dal.Repositories.Concrete;
 using DataTransfer.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DataTransfer.Business.Services.Concrete
 {
     public class FactoryService : RepositoryItm<Factory>, IFactoryService
     {
+        private AppDbContext _context;
         public FactoryService(AppDbContext context) : base(context)
         {
+            _context = context;
+        }
+        public List<Factory> GetAll()
+        {
+            var models = base.GetAll();
+            models = _context.Factories.Include(m => m.Country).ToList();
+
+            return models;
+        }
+        public async Task<Factory> GetAsync(int id)
+        {
+            var model = await base.GetAsync(id);
+            model = await _context.Factories.Include(m => m.Country).SingleOrDefaultAsync(m => m.Id == id);
+
+            return model;
+        }
+        public async Task<Factory> GetAsync(Expression<Func<Factory, bool>> filter)
+        {
+            var model = await base.GetAsync(filter);
+            model = await _context.Factories.Include(m => m.Country).SingleOrDefaultAsync(filter);
+
+            return model;
         }
     }
 }
