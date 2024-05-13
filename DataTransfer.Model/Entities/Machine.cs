f2558bf38034d53128d23e960762ed8b1deb2a55
İbrahim KAYA <ibrahim.kaya5466@gmail.com>
2024-05-13 11:13:02 +0300

    .

--- DataTransfer.Model/Entities/Machine.cs
+++ DataTransfer.Model/Entities/Machine.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public int MachineGroupId { get; set; }
         public Group MachineGroup { get; set; }
+        public bool? IsDeleted { get; set; } = false;
         public DateTime CreatedDate { get; set; }
     }
 }
